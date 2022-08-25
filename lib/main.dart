c639aba669d615c6f7db9a7fcaa76b71602b8496
14m3185yoshi <nytennis0713555555@gmail.com>
2022-08-25 18:00:50 +0900

    set up provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import './category_route.dart'; practice Number1(2022/08/25)
-import './todo_app.dart';
 
-// Provider
-// 最も基本的なProvider
-// StateProvider
-// 外部から変更可能な状態を提供する
-// StateNotifierProvider
-// 外部から変更可能な状態と、状態操作メソッドクラスを提供する
-// FutureProvider
-// 非同期で取得した値を提供する
-// StreamProvider
-// 断続的に最新の値を提供する
-// ChangeNotifierProvider
-// 変更可能な状態を持つクラスを提供する
+// 値（ここでは "Hello world"）を格納する「プロバイダ」を作成します。
+// プロバイダを使うことで値のモックやオーバーライドが可能になります。
+final helloWorldProvider = Provider((_) => 'Hello world');
 
 void main() {
-  runApp((ProviderScope(child: UnitConverterApp())));
+  runApp(
+    // プロバイダをウィジェットで利用するには、アプリ全体を
+    // `ProviderScope` ウィジェットで囲む必要があります。
+    // ここに各プロバイダのステート（状態）・値が格納されていきます。
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class UnitConverterApp extends StatelessWidget {
-  const UnitConverterApp({Key? key}) : super(key: key);
-
+// StatelessWidget の代わりに Riverpod の ConsumerWidget を継承します。
+class MyApp extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final String value = ref.watch(helloWorldProvider);
+
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Unit Converter',
-        theme: ThemeData(
-            textTheme: Theme.of(context)
-                .textTheme
-                .apply(bodyColor: Colors.black, displayColor: Colors.grey[600]),
-            primaryColor: Colors.grey[500],
-            textSelectionTheme: TextSelectionThemeData(
-                selectionHandleColor: Colors.green[500])),
-        home: TodoListPage());
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Example')),
+        body: Center(
+          child: Text(value),
+        ),
+      ),
+    );
   }
 }
